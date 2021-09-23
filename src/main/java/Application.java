a92689bf3e78af49317facdf7e20e96a374a774a
BAJANF <flavia-gabriela.bajan@pfizer.com>
2021-09-23 14:42:07 +0300

    Setter Getter injction

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,16 @@
 import com.conference.service.SpeakerService;
 import com.conference.service.SpeakerServiceImp;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
     public static void main(String args[]){
-        SpeakerService service=new SpeakerServiceImp();
+        ApplicationContext appContext=new AnnotationConfigApplicationContext(AppConfig.class) ;
+
+        //SpeakerService service=new SpeakerServiceImp();
+
+        SpeakerService service=appContext.getBean("speakerService", SpeakerService.class);
+
         System.out.println(service.findAll().get(0).getFirstName());
     }
 }
--- src/main/java/com/conference/service/SpeakerServiceImp.java
+++ src/main/java/com/conference/service/SpeakerServiceImp.java
@@ -7,11 +7,15 @@ import com.conference.repository.SpeakerRepository;
 import java.util.List;
 
 public class SpeakerServiceImp implements SpeakerService {
-    private SpeakerRepository repository=new HibernateSpeakerRepositoryImpl();
+    private SpeakerRepository repository;
 
     @Override
     public List<Speaker> findAll()
     {
         return repository.findAll();
+    }
+
+    public void setRepository(SpeakerRepository repository) {
+        this.repository = repository;
     }
 }
