3c946ee86d4f67745595318c6118fe4ad294c14c
BAJANF <flavia-gabriela.bajan@pfizer.com>
2021-11-29 09:27:19 +0200

    autowiring

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,24 +2,31 @@ import com.conference.repository.HibernateSpeakerRepositoryImpl;
 import com.conference.repository.SpeakerRepository;
 import com.conference.service.SpeakerService;
 import com.conference.service.SpeakerServiceImp;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan({"com.conference"})
 public class AppConfig {
-
-    @Bean(name="speakerService")
+/*
+    @Bean(name="spe akerService")
+    //@Scope(value = "singleton")
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
+    //@Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
     public SpeakerService getSpeakerService() {
         SpeakerServiceImp service= new SpeakerServiceImp(getSpeakerRepository());
+        //SpeakerServiceImp service=new SpeakerServiceImp();
         //service.setRepository(getSpeakerRepository());
         return service;
     }
 
-
     @Bean(name="speakerRepository")
     public SpeakerRepository getSpeakerRepository(){
         return  new HibernateSpeakerRepositoryImpl();
-    }
+    }*/
 
 
 
