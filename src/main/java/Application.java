875a7429f4ad8a94a4b40bc62703f38f6621c8ef
BAJANF <flavia-gabriela.bajan@pfizer.com>
2021-11-29 09:27:19 +0200

    autowiring

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,7 +9,11 @@ public class Application {
 
         //SpeakerService service=new SpeakerServiceImp();
 
+        //test singleton scope
         SpeakerService service=appContext.getBean("speakerService", SpeakerService.class);
+        System.out.println(service);
+        SpeakerService service2=appContext.getBean("speakerService", SpeakerService.class);
+        System.out.println(service2);
 
         System.out.println(service.findAll().get(0).getFirstName());
     }
--- src/main/java/com/conference/service/SpeakerServiceImp.java
+++ src/main/java/com/conference/service/SpeakerServiceImp.java
@@ -1,11 +1,19 @@
 package com.conference.service;
 
 import com.conference.model.Speaker;
-import com.conference.repository.HibernateSpeakerRepositoryImpl;
 import com.conference.repository.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
+@Scope(value = BeanDefinition.SCOPE_SINGLETON)
+@Service("speakerService")
 public class SpeakerServiceImp implements SpeakerService {
     private SpeakerRepository repository;
 
@@ -15,11 +23,18 @@ public class SpeakerServiceImp implements SpeakerService {
         return repository.findAll();
     }
 
+
     public void setRepository(SpeakerRepository repository) {
+        System.out.println("SpeakerServiceImp setter");
         this.repository = repository;
     }
-
+    @Autowired
     public SpeakerServiceImp(SpeakerRepository speakerRepository){
+        System.out.println("SpeakerServiceImp repository constructor");
         repository=speakerRepository;
+    }
+
+    public SpeakerServiceImp(){
+        System.out.println("SpeakerServiceImp no args constructor");
     }
 }
