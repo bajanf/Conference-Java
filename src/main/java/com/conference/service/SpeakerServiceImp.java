04e4bc9dd53ce4c9a8e6faa7bab3e3b8dfbfb7e5
BAJANF <flavia-gabriela.bajan@pfizer.com>
2021-12-02 10:31:37 +0200

    Advance Bean configuration - Factory Bean and SpEL bean profile

--- src/main/java/com/conference/service/SpeakerServiceImp.java
+++ src/main/java/com/conference/service/SpeakerServiceImp.java
@@ -4,6 +4,7 @@ import com.conference.model.Speaker;
 import com.conference.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Scope(value = BeanDefinition.SCOPE_SINGLETON)
 @Service("speakerService")
+@Profile("dev")
 public class SpeakerServiceImp implements SpeakerService {
     private SpeakerRepository repository;
 
